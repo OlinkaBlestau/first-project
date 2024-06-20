05f31309aaa3383d3eb1bdc093a1665227b56e91
OlinkaBlestau <alina.zahovora@nure.ua>
2024-06-20 22:55:22 +0300

    Project with API

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,106 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      city: "",
+      error: "",
+      info: null,
+    };
+  },
+  computed: {
+    cityName() {
+      return "'" + this.city + "'";
+    },
+
+    showTemp() {
+      return "Temperature: " + this.info.main.temp;
+    },
+    showFeelsLike() {
+      return "Feels like: " + this.info.main.feels_like;
+    },
+    showMinTemp() {
+      return "Min temperature: " + this.info.main.temp_min;
+    },
+    showMaxTemp() {
+      return "Max temperature: " + this.info.main.temp_max;
+    },
+  },
+  methods: {
+    getWeather() {
+      if (this.city.trim().length < 2) {
+        this.error = "Need more than 1 symbol";
+        return false;
+      }
+
+      this.error = "";
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=37d19ff7448566a5350530a52c605d6c`
+        )
+        .then((res) => (this.info = res.data));
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div className="wrapper">
+    <h1>Forecast weather</h1>
+    <p>Forecast for {{ city == "" ? "your city" : cityName }}</p>
+    <input v-model="city" type="text" placeholder="City" />
+    <button v-if="city != ''" @click="getWeather()">Search</button>
+    <p className="error">{{ error }}</p>
+    <div v-if="info != null">
+     <p>{{ showTemp }}</p>
+        <p>{{ showFeelsLike }}</p>
+        <p>{{ showMinTemp }}</p>
+        <p>{{ showMaxTemp }}</p>
+    </div> 
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 20px;
+  background: pink;
+  padding: 20px;
+  color: #d45c5c;
+  text-align: center;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper h1 {
+  margin-top: 50px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.wrapper p {
+  margin-top: 20px;
+}
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #fff;
+  color: #612121;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
+.wrapper input:focus {
+  border-bottom-color: #d45c5c;
+}
+.wrapper button {
+  background: #fff;
+  color: black;
+  border-radius: 10px;
+  border: 2px solid #fff;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
 }
 </style>
